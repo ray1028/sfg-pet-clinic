f4e29499981c49b4d7746e63523ce9aa1e963301
rayjiang <rayjiang@192.168.0.13>
2019-04-11 23:58:07 -0400

    Impl Map based Services, closes #6

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,52 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.services.CrudService;
+
+public class PetServiceMap extends AbstractMapSerivce<Pet, Long> implements CrudService<Pet, Long> {
+
+	@Override
+	public Set<Pet> findAll() {
+		// TODO Auto-generated method stub
+		return super.findAll();
+	}
+
+	@Override
+	public Pet findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public Pet save(Long id, Pet object) {
+		// TODO Auto-generated method stub
+		return super.save(id, object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Pet object) {
+		// TODO Auto-generated method stub
+		super.delete(object);
+	}
+
+	@Override
+	public Pet findById() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Pet save(Pet object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,53 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.CrudService;
+
+public class VetServiceMap extends AbstractMapSerivce<Vet, Long> 
+	implements CrudService<Vet, Long> {
+
+	@Override
+	public Set<Vet> findAll() {
+		// TODO Auto-generated method stub
+		return super.findAll();
+	}
+
+	@Override
+	public Vet findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public Vet save(Long id, Vet object) {
+		// TODO Auto-generated method stub
+		return super.save(id, object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Vet object) {
+		// TODO Auto-generated method stub
+		super.delete(object);
+	}
+
+	@Override
+	public Vet findById() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vet save(Vet object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
