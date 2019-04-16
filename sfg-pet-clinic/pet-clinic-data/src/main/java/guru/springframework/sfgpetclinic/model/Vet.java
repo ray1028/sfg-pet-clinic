8ab68a14dc7a7e25c51fc326340f37d0c003eb14
rayjiang <rayjiang@192.168.0.13>
2019-04-16 16:04:01 -0400

    added project lombok. close #51

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -10,6 +10,17 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name="vets")
 public class Vet extends Person {
@@ -21,12 +32,6 @@ public class Vet extends Person {
 	inverseJoinColumns = @JoinColumn(name = "speciality_id"))
 	private Set<Speciality> specialities = new HashSet<>();
 
-	public Set<Speciality> getSpecialities() {
-		return specialities;
-	}
 
-	public void setSpecialities(Set<Speciality> specialities) {
-		this.specialities = specialities;
-	}
 	
 }
